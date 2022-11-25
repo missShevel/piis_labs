083d4a63e14343864417da782093c9787c86c60b
Olha <o.v.shevel@gmail.com>
2022-11-25 19:58:23 +0000

    chess for monte carlo

---
+++ lab4/ChessGame.py
@@ -0,0 +1,199 @@
+from turtle import colormode
+import chess
+import random
+import time
+from math import log,sqrt,e,inf
+
+class AIEngine:
+    def __init__(self, board: chess.Board):
+        self.state = board
+        self.action = ''
+        self.children = set()
+        self.parent = None
+        self.N = 0
+        self.n = 0
+        self.v = 0
+
+
+def ucb1(curr_node):
+    ans = curr_node.v+2*(sqrt(log(curr_node.N+e+(10**-6))/(curr_node.n+(10**-10))))
+    return ans
+
+def rollout(curr_node):
+    
+    if(curr_node.state.is_game_over()):
+        board = curr_node.state
+        if(board.result()=='1-0'):
+            #print("h1")
+            return (1,curr_node)
+        elif(board.result()=='0-1'):
+            #print("h2")
+            return (-1,curr_node)
+        else:
+            return (0.5,curr_node)
+    
+    all_moves = [curr_node.state.san(i) for i in list(curr_node.state.legal_moves)]
+    
+    for i in all_moves:
+        tmp_state = chess.Board(curr_node.state.fen())
+        tmp_state.push_san(i)
+        child = AIEngine(chess.Board())
+        child.state = tmp_state
+        child.parent = curr_node
+        curr_node.children.add(child)
+    rnd_state = random.choice(list(curr_node.children))
+
+    return rollout(rnd_state)
+
+def expand(curr_node,white):
+    if(len(curr_node.children)==0):
+        return curr_node
+    max_ucb = -inf
+    if(white):
+        idx = -1
+        max_ucb = -inf
+        sel_child = None
+        for i in curr_node.children:
+            tmp = ucb1(i)
+            if(tmp>max_ucb):
+                idx = i
+                max_ucb = tmp
+                sel_child = i
+
+        return(expand(sel_child,0))
+
+    else:
+        idx = -1
+        min_ucb = inf
+        sel_child = None
+        for i in curr_node.children:
+            tmp = ucb1(i)
+            if(tmp<min_ucb):
+                idx = i
+                min_ucb = tmp
+                sel_child = i
+
+        return expand(sel_child,1)
+
+def rollback(curr_node,reward):
+    curr_node.n+=1
+    curr_node.v+=reward
+    while(curr_node.parent!=None):
+        curr_node.N+=1
+        curr_node = curr_node.parent
+    return curr_node
+
+def mcts_pred(curr_node,over,white,iterations=10):
+    if(over):
+        return -1
+    all_moves = [curr_node.state.san(i) for i in list(curr_node.state.legal_moves)]
+    map_state_move = dict()
+    
+    for i in all_moves:
+        tmp_state = chess.Board(curr_node.state.fen())
+        tmp_state.push_san(i)
+        child = AIEngine(chess.Board())
+        child.state = tmp_state
+        child.parent = curr_node
+        curr_node.children.add(child)
+        map_state_move[child] = i
+        
+    while(iterations>0):
+        if(white):
+            idx = -1
+            max_ucb = -inf
+            sel_child = None
+            for i in curr_node.children:
+                tmp = ucb1(i)
+                if(tmp>max_ucb):
+                    idx = i
+                    max_ucb = tmp
+                    sel_child = i
+            ex_child = expand(sel_child,0)
+            reward,state = rollout(ex_child)
+            curr_node = rollback(state,reward)
+            iterations-=1
+        else:
+            idx = -1
+            min_ucb = inf
+            sel_child = None
+            for i in curr_node.children:
+                tmp = ucb1(i)
+                if(tmp<min_ucb):
+                    idx = i
+                    min_ucb = tmp
+                    sel_child = i
+
+            ex_child = expand(sel_child,1)
+
+            reward,state = rollout(ex_child)
+
+            curr_node = rollback(state,reward)
+            iterations-=1
+    if(white):
+        mx = -inf
+        idx = -1
+        selected_move = ''
+        for i in (curr_node.children):
+            tmp = ucb1(i)
+            if(tmp>mx):
+                mx = tmp
+                selected_move = map_state_move[i]
+        return selected_move
+    else:
+        mn = inf
+        idx = -1
+        selected_move = ''
+        for i in (curr_node.children):
+            tmp = ucb1(i)
+            if(tmp<mn):
+                mn = tmp
+                selected_move = map_state_move[i]
+        return selected_move
+
+
+
+class GameEngine:
+    def __init__(self, board: chess.Board):
+        self.board = board
+
+    def playHumanMove(self):
+        print("Possible moves: ", self.board.legal_moves)
+        play = input("Enter your move: ")
+        self.board.push_san(play)
+
+    def playAIMove(self, color, isOver):
+        engine = AIEngine(self.board)
+
+        bestMove = mcts_pred(engine, isOver, color)
+            
+        print('BEST MOVE', bestMove)
+        self.board.push_san(bestMove)
+        return
+
+    def startGame(self):
+        aiColor = chess.BLACK
+        print("The game started!")
+        print("You play WHITE!")
+
+        turn = chess.WHITE
+
+        while (not self.board.is_checkmate()):
+            print(self.board)
+            if turn == chess.WHITE:
+                print('\n\nWhite move\n\n')
+                self.playHumanMove()
+                turn = chess.BLACK
+                continue
+            if turn == chess.BLACK:
+                print('\n\nBlack move\n\n')
+                self.playAIMove(-aiColor, self.board.is_checkmate())
+                turn = chess.WHITE
+                continue
+        return
+
+    
+if __name__ == '__main__':
+    game = GameEngine(chess.Board())
+    
+    game.startGame()
